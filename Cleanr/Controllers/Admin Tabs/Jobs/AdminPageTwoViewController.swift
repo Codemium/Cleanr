d7e0ef67b2e0e4c9642b5ca75dafa3ab74f24343
vishwajeet <vishwajeet@vishwajeets-macbook-air.local>
2018-08-18 22:03:49 +0530

    Basic layout and tableview of 'Jobs' tab prepared.

--- Cleanr/Controllers/Admin Tabs/Jobs/AdminPageTwoViewController.swift
+++ Cleanr/Controllers/Admin Tabs/Jobs/AdminPageTwoViewController.swift
@@ -8,19 +8,68 @@
 
 import UIKit
 
-class AdminPageTwoViewController: UIViewController {
+class AdminPageTwoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    var jobNameArray : [String] = ["Window cleaning","Carpet cleaning","House cleaning"]
+    
+    var dateArray = ["10-11-2018","12-5-2018","1-4-2018"]
+    
+    var clientNameArray = ["Hritik Roshan","Rutvik Roshan","Ritvik Roshan"]
+    
+    @IBOutlet weak var tableOutlet: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableOutlet.delegate = self
+        tableOutlet.dataSource = self
+        
+        tableOutlet.separatorStyle = .none
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
     
- 
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        var sectionName = ""
+        
+        if section == 0 {
+            sectionName = "Upcoming"
+        }
+        if section == 1 {
+            sectionName = "Completed"
+        }
+        
+        return sectionName
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return jobNameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customJobsCell", for: indexPath) as! CustomJobsCell
+        
+        cell.jobName.text = jobNameArray[indexPath.row]
+        
+        cell.date.text = dateArray[indexPath.row]
+        
+        cell.clientName.text = clientNameArray[indexPath.row]
+        
+        tableView.rowHeight = 103
+        
+        return cell
+        
+    }
+    
+
 
 }
---
+++ Cleanr/Custom Cell/CustomJobsCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomJobsCell.swift
+//  Cleanr
+//
+//  Created by vishwajeet on 18/08/18.
+//  Copyright © 2018 vishwajeet jadeja. All rights reserved.
+//
+
+import UIKit
+
+class CustomJobsCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var jobName: UILabel!
+    @IBOutlet weak var clientName: UILabel!
+    @IBOutlet weak var date: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
